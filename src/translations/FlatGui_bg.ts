ac42111360b64d603111242cb9372acf4726cf79
Michael Scopchanov <michael_scopchanov@yahoo.com>
2018-09-26 21:20:40 +0200

    Added the translation files, as well as the files needed for documentation.

---
+++ src/translations/FlatGui_bg.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg_BG">
+<context>
+    <name>GenericForm</name>
+    <message>
+        <location filename="../GenericForm.cpp" line="58"/>
+        <source>The fields marked with %1 need to be filled out. The rest of the fields are either optional or already contain information.</source>
+        <translation>Полетата маркирани с %1 трябва да бъдат попълнени. Останалите полета или не са задължителни, или вече съдържат информация.</translation>
+    </message>
+</context>
+<context>
+    <name>SearchForm</name>
+    <message>
+        <location filename="../SearchForm.cpp" line="44"/>
+        <source>Enter search term</source>
+        <translation>Въведете текст за търсене</translation>
+    </message>
+</context>
+<context>
+    <name>TimeEditor</name>
+    <message>
+        <location filename="../TimeEditor.cpp" line="48"/>
+        <source>Hours</source>
+        <translation>Часове</translation>
+    </message>
+    <message>
+        <location filename="../TimeEditor.cpp" line="49"/>
+        <source>Minutes</source>
+        <translation>Минути</translation>
+    </message>
+    <message>
+        <location filename="../TimeEditor.cpp" line="76"/>
+        <source>Cancel</source>
+        <translation>Откажи</translation>
+    </message>
+    <message>
+        <location filename="../TimeEditor.cpp" line="77"/>
+        <source>Apply</source>
+        <translation>Приложи</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/FlatGui_es.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>GenericForm</name>
+    <message>
+        <location filename="../GenericForm.cpp" line="58"/>
+        <source>The fields marked with %1 need to be filled out. The rest of the fields are either optional or already contain information.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchForm</name>
+    <message>
+        <location filename="../SearchForm.cpp" line="44"/>
+        <source>Enter search term</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TimeEditor</name>
+    <message>
+        <location filename="../TimeEditor.cpp" line="48"/>
+        <source>Hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TimeEditor.cpp" line="49"/>
+        <source>Minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TimeEditor.cpp" line="76"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TimeEditor.cpp" line="77"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
