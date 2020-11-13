1ad7cbe4bd856bcdd6ffbfdbe8ff22f1bc06097b
Michael Scopchanov <scopchanov@users.noreply.github.com>
2020-11-13 06:00:37 +0100

    The split view can now be placed on the left hand side as well as on the right hand side of the window.

--- src/SplitView.cpp
+++ src/SplitView.cpp
@@ -1,35 +1,6 @@
-/**
-MIT License
-
-Copyright (c) 2018 Michael Scopchanov
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 #include "SplitView.h"
 #include "SplitView_p.h"
 #include <QVariantAnimation>
-
-/*!
- * \class SplitView
- * \brief Allows the screen to be split in two
- */
 
 SplitView::SplitView(QWidget *parent) :
 	QWidget(parent),
@@ -61,11 +32,22 @@ void SplitView::setSideWidget(QWidget *widget, bool hidden)
 	m_ptr->sideWidget = widget;
 	m_ptr->slideIn = hidden;
 
-	if (hidden)
-		m_ptr->sideWidget->move(width(), 0);
+	m_ptr->doLayout(m_ptr->splitSide == ST_Left
+					? (hidden ? -m_ptr->sideWidget->width() : 0)
+					: (hidden ? width() : width() - m_ptr->sideWidget->width()));
 }
 
-void SplitView::splitScreen()
+SplitView::SideType SplitView::splitSide() const
+{
+	return static_cast<SideType>(m_ptr->splitSide);
+}
+
+void SplitView::setSplitSide(SplitView::SideType sd)
+{
+	m_ptr->splitSide = sd;
+}
+
+void SplitView::toggleSideWidget()
 {
 	if (m_ptr->busy || !m_ptr->sideWidget)
 		return;
@@ -74,15 +56,20 @@ void SplitView::splitScreen()
 
 	m_ptr->busy = true;
 
-	animation->setStartValue(m_ptr->slideIn ? 0 : m_ptr->sideWidget->width());
-	animation->setEndValue(m_ptr->slideIn ? m_ptr->sideWidget->width() : 0);
+	animation->setStartValue(m_ptr->splitSide == ST_Left
+							 ? (m_ptr->slideIn ? -m_ptr->sideWidget->width() : 0)
+							 : (m_ptr->slideIn ? 0 : m_ptr->sideWidget->width()));
+	animation->setEndValue(m_ptr->splitSide == ST_Left
+						   ? (m_ptr->slideIn ? 0 : -m_ptr->sideWidget->width())
+						   : (m_ptr->slideIn ? m_ptr->sideWidget->width() : 0));
 	animation->setDuration(150);
-	animation->start(QVariantAnimation::DeleteWhenStopped);
 
 	connect(animation, &QVariantAnimation::valueChanged,
-			this, &SplitView::onSplitChanged);
+			this, &SplitView::onValueChanged);
 	connect(animation, &QVariantAnimation::finished,
-			this, &SplitView::onSplitFinished);
+			this, &SplitView::onAnimationFinished);
+
+	animation->start(QVariantAnimation::DeleteWhenStopped);
 }
 
 void SplitView::resizeEvent(QResizeEvent *)
@@ -90,38 +77,47 @@ void SplitView::resizeEvent(QResizeEvent *)
 	if (!m_ptr->sideWidget)
 		return;
 
-	m_ptr->setHorizontalSplit(width() - (m_ptr->slideIn
-										 ? 0 : m_ptr->sideWidget->width()));
+	m_ptr->doLayout(m_ptr->splitSide == ST_Left
+					? (m_ptr->slideIn ? -m_ptr->sideWidget->width() : 0)
+					: (m_ptr->slideIn ? 0: m_ptr->sideWidget->width()));
 }
 
-void SplitView::onSplitChanged(const QVariant &value)
+void SplitView::onValueChanged(const QVariant &value)
 {
-	m_ptr->setHorizontalSplit(width() - value.toInt());
+	m_ptr->doLayout(value.toInt());
 }
 
-void SplitView::onSplitFinished()
+void SplitView::onAnimationFinished()
 {
 	m_ptr->slideIn ^= true;
 	m_ptr->busy = false;
+
+	toggled(!m_ptr->slideIn);
 }
 
 SplitViewPrivate::SplitViewPrivate(SplitView *parent) :
 	p_ptr(parent),
 	baseWidget(nullptr),
 	sideWidget(nullptr),
+	splitSide(SplitView::ST_Left),
 	busy(false),
 	slideIn(false)
 {
 
 }
 
-void SplitViewPrivate::setHorizontalSplit(int x)
+void SplitViewPrivate::doLayout(int x)
 {
 	if (!baseWidget || !sideWidget)
 		return;
 
 	int h = p_ptr->height();
+	int w = p_ptr->width() - x;
+	bool leftSide = splitSide == SplitView::ST_Left;
 
-	baseWidget->setGeometry(0, 0, x, h);
+	x = leftSide ? x : w;
+
+	baseWidget->setGeometry(leftSide ? x + sideWidget->width() : 0, 0,
+							leftSide ? w - sideWidget->width() : w, h);
 	sideWidget->setGeometry(x, 0, sideWidget->width(), h);
 }
--- src/SplitView.h
+++ src/SplitView.h
@@ -1,47 +1,29 @@
-/**
-MIT License
-
-Copyright (c) 2018 Michael Scopchanov
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 #ifndef SPLITVIEW_H
 #define SPLITVIEW_H
 
-#include "flatgui_global.h"
 #include <QWidget>
 
 class SplitViewPrivate;
 
-class FLATGUISHARED_EXPORT SplitView : public QWidget
+class SplitView : public QWidget
 {
 	Q_OBJECT
 public:
+	enum SideType : int {
+		ST_Left,
+		ST_Right
+	};
+
 	explicit SplitView(QWidget *parent = nullptr);
 	~SplitView();
 
 	void setBaseWidget(QWidget *widget);
 	void setSideWidget(QWidget *widget, bool hidden = true);
+	SideType splitSide() const;
+	void setSplitSide(SideType sd);
 
 public slots:
-	void splitScreen();
+	void toggleSideWidget();
 
 protected:
 	void resizeEvent(QResizeEvent *) override;
@@ -50,8 +32,11 @@ private:
 	SplitViewPrivate *m_ptr;
 
 private slots:
-	void onSplitChanged(const QVariant &value);
-	void onSplitFinished();
+	void onValueChanged(const QVariant &value);
+	void onAnimationFinished();
+
+signals:
+	void toggled(bool isOpen);
 };
 
 #endif // SPLITVIEW_H
